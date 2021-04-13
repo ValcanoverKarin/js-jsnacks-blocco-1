a650fcd82db6faf0b2bdbde830dc715847bab16c
karin valcanover <valcanoverkarin@outlook.it>
2021-04-13 16:21:35 +0200

    creato 10 oggetti

--- js/script.js
+++ js/script.js
@@ -4,12 +4,67 @@
 //   Dividi in due array separati le zucchine che misurano meno o più di 15cm. 
 //3. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
 
-//1a. 
+//1a. creo 10 oggetti con varietà peso e lunghezza in un array 
 var arrayZucchine = [
     {
         //zucchina 0
-        'varieta' : 'Michela',
+        'varieta' : 'Zucchino nero',
         'peso' : 3,
         'lunghezza' : 18
     },
-]
+    {
+        //zucchina 1
+        'varieta' : 'Zucchino giallo',
+        'peso' : 2,
+        'lunghezza' : 12
+    },
+    {
+        //zucchina 2
+        'varieta' : 'Zucchino nero',
+        'peso' : 1,
+        'lunghezza' : 14
+    },
+    {
+        //zucchina 3
+        'varieta' : 'Zucchina bianca',
+        'peso' : 2,
+        'lunghezza' : 13
+    },
+    {
+        //zucchina 4
+        'varieta' : 'Zucchina bianca',
+        'peso' : 4,
+        'lunghezza' : 11
+    },
+    {
+        //zucchina 5
+        'varieta' : 'Zucchino nero',
+        'peso' : 3,
+        'lunghezza' : 16
+    },
+    {
+        //zucchina 6
+        'varieta' : 'Zucchino giallo',
+        'peso' : 2,
+        'lunghezza' : 15
+    },
+    {
+        //zucchina 7
+        'varieta' : 'Zucchino nero',
+        'peso' : 4,
+        'lunghezza' : 16
+    },
+    {
+        //zucchina 8
+        'varieta' : 'Zucchina bianca',
+        'peso' : 1,
+        'lunghezza' : 17
+    },
+    {
+        //zucchina 9
+        'varieta' : 'Zucchino giallo',
+        'peso' : 3,
+        'lunghezza' : 10
+    },
+];
+console.log(arrayZucchine);
