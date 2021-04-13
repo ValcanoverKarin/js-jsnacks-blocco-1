8a5a38e45312d74599c0f88e199d918c22d1088b
karin valcanover <valcanoverkarin@outlook.it>
2021-04-13 16:11:10 +0200

    html js inizio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//1. Crea 10 oggetti che rappresentano una zucchina, indicandone per ognuno varietà, peso e lunghezza. 
+//   Calcola la somma del peso di tutte le zucchine.
+//2, Crea 10 oggetti che rappresentano una zucchina.
+//   Dividi in due array separati le zucchine che misurano meno o più di 15cm. 
+//3. Infine stampa separatamente quanto pesano i due gruppi di zucchine.
+
+//1a. 
+var arrayZucchine = [
+    {
+        //zucchina 0
+        'varieta' : 'Michela',
+        'peso' : 3,
+        'lunghezza' : 18
+    },
+]
